cd0ed234f962d3cbd8581918c4ac757305a0a3dd
Sashinee M <sashineemohanraj@gmail.com>
2026-07-06 21:24:05 +0530

    increasing

--- C/Exercise/non_decreasing_array_t_or_f.c
+++ C/Exercise/non_decreasing_array_t_or_f.c
@@ -5,13 +5,12 @@
 
 bool isSorted(int *arr, int n) {
     // write your solution below
-    for(int i=0; i<n-1; i++){
-        if(arr[i]<=arr[i+1]){
+    for(int i= 0; i<n-1; i++){
+        if(arr[i]>arr[i+1]){
             return false;
-        }
+        } 
     }
-
-
+return true;
 }
 
 
