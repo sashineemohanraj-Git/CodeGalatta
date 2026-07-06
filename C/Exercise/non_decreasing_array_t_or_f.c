d604394b36e89c824ebf004310a137739781bd1c
Sashinee M <sashineemohanraj@gmail.com>
2026-07-06 21:17:00 +0530

    increasing-orderarray

--- C/Exercise/non_decreasing_array_t_or_f.c
+++ C/Exercise/non_decreasing_array_t_or_f.c
@@ -6,7 +6,7 @@
 bool isSorted(int *arr, int n) {
     // write your solution below
     for(int i=0; i<n-1; i++){
-        if(arr[i]arr[i+1]){
+        if(arr[i]<=arr[i+1]){
             return false;
         }
     }
