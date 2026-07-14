e498913bde07606a4ac108061c4237e3ba4c9abd
Sashinee M <sashineemohanraj@gmail.com>
2026-07-14 21:09:18 +0530

    to access particular index in string

---
+++ C++/Basic_PRogram/to_access_string.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+
+    string num =" hello world";
+
+    cout << num[4] << endl;
+
+    return 0;
+}
---
+++ C++/Basic_PRogram/to_find_size_of_string.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+
+    string num= "hello";
+    cout<< num. size() << endl;
+
+    return 0;
+}
