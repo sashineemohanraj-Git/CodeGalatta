52aa85b12767ee693379d8476b761f0fa9c5a392
Sashinee M <sashineemohanraj@gmail.com>
2026-07-15 21:47:48 +0530

    to access in which index is letter l found

--- C++/loops_program/for_each_loop.cpp
+++ C++/loops_program/for_each_loop.cpp
@@ -4,10 +4,15 @@ using namespace std;
 int main(){
 
     string greeting = "Hello World";
+    int index = 0;
     
     //for each loop
     for( char value: greeting){
+        if (value== 'l'){
+            cout << "The letter e is found at index: " << index << endl;
+        }
         cout << value << endl;
+        index++;
     }
 
     return 0;
