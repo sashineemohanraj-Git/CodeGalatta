a7b7d27077a29b8bc9e5166df27d9926a1efc386
Sashinee M <sashineemohanraj@gmail.com>
2026-07-15 21:36:00 +0530

    to access elements in string using for each loop

---
+++ C++/loops_program/for_each_loop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+
+    int num[] = { 6, 7, 29, 5, 4, 3, 2, 1, 0 };
+
+    int length = sizeof(num) / sizeof(num[0]);
+
+    
+    //for each loop
+    for( int value: num){
+        cout << value << endl;
+    }
+
+    return 0;
+}
