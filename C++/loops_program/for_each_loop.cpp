4bb659becea49a314c22d183abedca81e6792c96
Sashinee M <sashineemohanraj@gmail.com>
2026-07-15 21:44:50 +0530

    for each loop for array and strings

--- C++/loops_program/for_each_loop.cpp
+++ C++/loops_program/for_each_loop.cpp
@@ -3,14 +3,11 @@
 using namespace std;
 int main(){
 
-    int num[] = { 6, 7, 29, 5, 4, 3, 2, 1, 0 };
-
-    int length = sizeof(num) / sizeof(num[0]);
-
+    string greeting = "Hello World";
     
     //for each loop
-    for( int value: num){
-        cout << value +1 << endl;
+    for( char value: greeting){
+        cout << value << endl;
     }
 
     return 0;
