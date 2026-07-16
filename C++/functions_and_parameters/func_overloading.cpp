956f9765d8db07525f8dc819523e7dc4f297fce0
Sashinee M <sashineemohanraj@gmail.com>
2026-07-16 21:34:06 +0530

    same func name diff no of parameters

--- C++/functions_and_parameters/func_overloading.cpp
+++ C++/functions_and_parameters/func_overloading.cpp
@@ -6,7 +6,9 @@ void print(int x){
     cout << x << endl;
 }
 
-void print(int x, int y){
+void print(int x, int y){ 
+    // function overloading with two parameters cannot be defined with default parameters as error will be thrown 
+    //but we can default two parameters in a function with two parameters
     cout << x << " " << y << endl;
 }
 int main(){
