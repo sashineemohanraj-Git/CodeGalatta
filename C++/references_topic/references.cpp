440650dbb6929e79a4faae0b4605cc8c81aab7f3
Sashinee M <sashineemohanraj@gmail.com>
2026-07-16 21:59:11 +0530

    ptr and dereferencing

--- C++/references_topic/references.cpp
+++ C++/references_topic/references.cpp
@@ -6,7 +6,7 @@ int main(){
 int num=10;
 // pointer variable- variable that stores the address of another variable
 int *ptr=&num;
-
+// dereferencing pointer variable- accessing the value of the variable whose address is stored in the pointer variable(*ptr)
 cout<< "Value of num: "<< *ptr << endl;
     return 0;
 }
