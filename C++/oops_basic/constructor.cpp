ead3ff3cd7b8eb5d7da5d8a9894dc35db183a92e
Sashinee M <sashineemohanraj@gmail.com>
2026-07-21 22:01:45 +0530

    void display

--- C++/oops_basic/constructor.cpp
+++ C++/oops_basic/constructor.cpp
@@ -12,7 +12,7 @@ class person{
     /*constructor- spl method that is automatically called when object of class is created(only in public)
     no return type like no void int or anything
     */
-   //use of constructor
+   //use of constructor- to give properties and arguments for the class instead of assigning every for every single object
    //classvariable(parameters){}
    
       person(string n, int a, int h){
@@ -22,13 +22,26 @@ class person{
 
     cout<< "sashinee constructor"<< endl;
       } 
+
+      //to print the properties in the object
+      void display(){
+        cout<< "Name:"<< name<< endl;
+        cout<< "Age:"<< age<< endl;
+        cout<< "height:"<< height<< endl;
+      }
 } ;
 
 int main(){
 
     person person1("sashinee", 18, 154);
+    person person2("mohan", 48, 167);
     
-    
+    /*to print the data inside the object
+    person1.display();
+    cout<< endl;
+    */
+    person1.display();
+    cout<<endl;
 
     return 0;
 }
