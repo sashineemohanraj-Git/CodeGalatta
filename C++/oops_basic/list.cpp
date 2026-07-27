53350a75ae29da70c963117545b3a396a138049c
Sashinee M <sashineemohanraj@gmail.com>
2026-07-27 21:13:21 +0530

    list and vector

---
+++ C++/oops_basic/list.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <list>
+
+//in list u can add and remove elements at beginning and end but in vector u can only add and remove only at end.
+// in list u cant access elements using index.
+using namespace std;
+
+int main(){
+
+vector<int>nums= {7,48,36};
+
+list<int>marks={80, 76, 90, 68, 97};
+
+
+
+list<string> fruits;
+
+cout<< marks.front()<< endl;
+
+//push_back and pop_back;
+//push_front and pop_front;
+    return 0;
+}
+
