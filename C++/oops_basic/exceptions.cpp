b367867c02d67427d46c5431bab354e5bb7405c9
Sashinee M <sashineemohanraj@gmail.com>
2026-07-29 21:23:25 +0530

    exceptions handling

---
+++ C++/oops_basic/exceptions.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    //exceptions not in c programming.
+    //exceptions- instead of errror occuring we handle it by other ways.
+
+    int a= 6 ; //some random no comes
+int b =0;
+
+if (b ==0){
+   cout<< "b is zero"<< endl;
+} else {
+int num = a/b;
+    cout<< num << endl;
+}
+
+    return 0;
+}
+
