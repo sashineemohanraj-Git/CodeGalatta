db6812b7147d65133082073c654a0a9e2dcf7d9b
Sashinee M <sashineemohanraj@gmail.com>
2026-08-12 22:06:36 +0530

    slicing negative step

---
+++ Python/string/slicing_negative.py
@@ -0,0 +1,19 @@
+
+text= "following"
+
+print(text[0])
+
+'''if step is negative(-1)
+if start is not mentioned then it will start from the end of the string
+if end is not mentioned then it will go till the start of the string'''
+
+print(text[:0:-1]) #here we get gninwoll
+
+print(text[8:2:-2]) #here we get gio
+
+print(text[3::-1]) #here we get llof
+
+print(text[:15:1]) #following
+
+print(text[:-15:-1]) #we get following in reverse order.
+
