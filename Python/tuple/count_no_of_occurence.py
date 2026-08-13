7511479aae49ee55e5016e1ebc7ec337c0f5be43
Sashinee M <sashineemohanraj@gmail.com>
2026-08-13 22:10:52 +0530

    index

---
+++ Python/tuple/count_no_of_occurence.py
@@ -0,0 +1,5 @@
+fruits= ("apple", "mango", "mango")
+
+print(fruits.count("mango")) #counting the number of times mango is present in the list
+
+print(fruits.count("banana")) #counting the number of times banana is present in the list
---
+++ Python/tuple/index.py
@@ -0,0 +1,6 @@
+fruits= ("apple", "mango", "mango")
+
+print(fruits.index("mango")) #index of the first occurrence of "mango"
+print(fruits.count("banana")) #counting the number of times banana is present in the list
+
+print(fruits.index("apple"))
