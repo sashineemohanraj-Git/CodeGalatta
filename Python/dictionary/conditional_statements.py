fe46bb93732c7a9ac42ddcb44d33d81e924294e7
Sashinee M <sashineemohanraj@gmail.com>
2026-08-18 22:13:12 +0530

    if

---
+++ Python/dictionary/conditional_statements.py
@@ -0,0 +1,13 @@
+#conditional statements
+'''if condition:
+elif condition:
+else:'''
+
+s= 7
+
+if s > 3:
+    pass # if pass is used to avoid indentation error when we don't want to write any code in if block
+elif s < 3:
+    print("s is less than 3")
+else:
+    print("s is equal to 3")
