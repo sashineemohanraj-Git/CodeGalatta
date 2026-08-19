1ae8f81b6a0a8820af713bd814d467c01538af11
Sashinee M <sashineemohanraj@gmail.com>
2026-08-19 22:13:25 +0530

    loops'

---
+++ Python/loops/for_while.py
@@ -0,0 +1,13 @@
+fruits={"apple", "banana", "cherry"}
+
+for fruit in fruits:
+    if(fruit== "watermelon"):
+      print("fruit is watermelon")
+      break
+    else:
+      print("fruit is not watermelon")
+
+    num=5
+    while(num>2):
+          print(num)
+          num-=1
---
+++ Python/loops/tuple_loop.py
@@ -0,0 +1,3 @@
+fruits= [("apple", "red"), ("banana", "yellow"), ("grape", "purple")]
+for(key, value) in fruits:
+    print(f"The {key} is {value}.")
